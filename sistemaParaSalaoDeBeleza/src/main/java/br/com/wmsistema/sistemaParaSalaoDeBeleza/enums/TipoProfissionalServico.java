fbda11cfebbb2a0442c0e5636ab21c35b256a688
Wellington-Yoshdia <issami-yoshida@hotmail.com>
2018-12-20 06:00:18 -0200

    Criação de alguns cargos

--- sistemaParaSalaoDeBeleza/src/main/java/br/com/wmsistema/sistemaParaSalaoDeBeleza/enums/TipoProfissionalServico.java
+++ sistemaParaSalaoDeBeleza/src/main/java/br/com/wmsistema/sistemaParaSalaoDeBeleza/enums/TipoProfissionalServico.java
@@ -5,7 +5,17 @@ public enum TipoProfissionalServico {
     CABELEREIRA("cabelereira(o)"),
     MANICURI("manicuri"),
     MAQUIADORA("maquiadora"),
-    AUXILIAR("auxiliar");
+    AUXILIAR("auxiliar"),
+    ESTETICISTA("esteticista"),
+    BARBEIRO("barbeiro"),
+    RECEPCIONISTA("recepcionista"),
+    SERVICOS_GERAIIS("serviços gerais"),
+    AUMOXARIFADO("almoxarifado"),
+    GERENTE("gerente"),
+    DESIGN_UNHAS("design de unhas"),
+    DESIGN_SOBRANCELHA("design de sibrancelhas"),
+    MASSAGISTA("massagista"),
+    PODOLODA("podologa");
 
     private String valor;
     TipoProfissionalServico(String s) {
